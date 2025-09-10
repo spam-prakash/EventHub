43aafea3f5e5ba11c878cabd6e024f83b558cd29
Prakash Kumar <dev.prakash.spam@gmail.com>
2025-09-10 11:41:25 +0530

    Create Event.js

---
+++ backend/models/Event.js
@@ -0,0 +1,35 @@
+const mongoose= require('mongoose')
+
+const EventSchema= new mongoose.Schema({
+  title:{
+    type:String,
+    required: True
+  },
+  host:{
+    type:String,
+    required: true
+  },
+  location:{
+    type: String,
+    required: true
+  },
+  time:{
+    type: String,
+    required: true
+  },
+  link:{
+    type: String
+  },
+  desc:{
+    type: String
+  },
+  category:{
+    type: String,
+    required: true
+  },
+  actions:{
+    wishlist:[{type: mongoose.Schema.Types.ObjectId,ref: "User"}],
+    rsvp:[{type: mongoose.Schema.Types.ObjectId,ref: "User"}],
+    attendees:[{type: mongoose.Schema.Types.ObjectId,ref: "User"}],
+  }
+})
